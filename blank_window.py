ee1b08649573987600d3968c473553c855170c59
SonYongHun <son0002136100@gmail.com>
2025-07-01 16:57:13 +0900

    다양한 게임들 추가

--- blank_window.py
+++ blank_window.py
@@ -7,13 +7,18 @@ from PyQt5.QtWidgets import QApplication, QWidget
 class MyApp(QWidget):
 
     def __init__(self):
+        #super -> 부모 클래스
         super().__init__()
         self.initUI()
 
     def initUI(self):
-        self.setWindowTitle('My First Application')
-        self.move(300, 300)
-        self.resize(400, 200)
+        #창이름
+        self.setWindowTitle('WindowTitle')
+        #창위치
+        self.move(0, 0)
+        #창크기
+        self.resize(1280, 720)
+        #창띄우기
         self.show()
 
 
@@ -21,3 +26,4 @@ if __name__ == '__main__':
    app = QApplication(sys.argv)
    ex = MyApp()
    sys.exit(app.exec_())
+
